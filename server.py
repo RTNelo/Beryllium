63f3a1fca4436ec43392308c375a6a8abdccd5cd
RTNelo <rtnelo@yeah.net>
2013-08-03 18:03:14 +0800

    Add the server.py and some code and docstring inserted.
    Create and start a HTTPServer instance which will listen the 80 port in the
    server.py. And introduce the blog package (just application module).

---
+++ server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Beryllium is a simple blog application. And the Be-5 version is the lightest
+one.
+
+The server.py will start a HTTPServer instance and make it listen the 80 port.
+So you can use `python server.py` to start you httpserver (and the blog).
+
+The blog package is the blog application itself. The blog.application defines
+the the Application class used by the blog application.
+"""
+
+from tornado import httpserver
+from tornado import ioloop
+
+from blog import application
+
+
+def main():
+    """Main fuction which will create a HTTPServer listening the 80 port And
+    start it."""
+    http_server = httpserver.HTTPServer(application.Application)
+    http_server.listen(80)
+    ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    main()
