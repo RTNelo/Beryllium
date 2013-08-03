3fe7b9ae3495a4f20251119d67b8bcf7d5fa4fab
RTNelo <rtnelo@yeah.net>
2013-08-03 18:33:11 +0800

    Add the urls.py and a little docstring in the server.py(about the urls).
    The urls.urls is empty now. Because the handlers.py have been not
    created.

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+The urls module defines the urls used by the blog app.
+
+urls declare the relationship between the handlers and the urls. In another
+word, it defines a url should be handled by which handler.
+
+Now, the urls is empty yet. Because the handlers have been not writed.
+"""
+import handlers  # This module defines the handlers used by the blog app.
+
+urls = [(),
+        ]
