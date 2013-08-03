3618c7a02c7c169ef0aa01b4a2707c4bffdf63ef
RTNelo <rtnelo@yeah.net>
2013-08-03 18:23:09 +0800

    Add the application.py of the blog package.
    It defines the Application class used by the blog app. Now it just very
    simple (only use the urls.urls(will be added in the futhure) to
    initialize).

---
+++ blog/application.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+The application module define the Application class used by the blog app.
+
+Application will use the urls in the urls module to initialize it self.
+"""
+from tornado import web
+
+import urls  # This module defines the urls.
+
+
+class Application(web.Application):
+    def __init__(self):
+        """The Application class used by the blog application. It will prepare
+        something for the blog app.
+        Now it just create an alias of urls.urls and use it to initialize.
+        """
+        app_urls = urls.urls
+
+        super(Application, self).__init__(app_urls)
