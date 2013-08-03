b2e1271db20bfa4e970be2f0fc9a956485026d9e
RTNelo <rtnelo@yeah.net>
2013-08-03 20:31:12 +0800

    Add a mako.lookup.TemplateLookup instance into the Application.

--- blog/application.py
+++ blog/application.py
@@ -5,7 +5,10 @@ The application module define the Application class used by the blog app.
 
 Application will use the urls in the urls module to initialize it self.
 """
+import tempfile
+
 from tornado import web
+from mako import lookup
 
 import urls  # This module defines the urls.
 
@@ -17,5 +20,16 @@ class Application(web.Application):
         Now it just create an alias of urls.urls and use it to initialize.
         """
         app_urls = urls.urls
+
+        #Prepare the TemplateLookup
+        self.template_lookup = lookup.TemplateLookup(
+            ['templates/'],  # Path to look up templates.
+            module_dictionary=tempfile.mkdtemp(),  # Create a temp directory to
+                                                   # store compiled templates.
+            #TODO: Use options' value.
+            filesystem_checkes=True,  # Track the template file, when it
+                                      # modified, reload it.
+            input_encoding='utf-8',  # Encoding of the template files.
+        )
 
         super(Application, self).__init__(app_urls)
