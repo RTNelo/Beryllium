081f6001bcd8ec0d078f96e6a2335b7f7ea63005
RTNelo <rtnelo@yeah.net>
2013-08-05 12:23:55 +0800

    Remove almost all docstring in the blog/__init__.py.

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,26 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Beryllium is a simple blog application. And the Be-5 version is the lightest
-one.
+"""Beryllium is a simple blog application based on Tornado."""
 
-The blog package is the blog application itself.
-
-The blog.application defines the the Application class used by the blog
-application.
-
-The blog.urls defines the relationship between the handlers and the urls.
-
-The blog.handlers defines the handlers used in the app.
-
-The blog.context defines some useful instance used by the app, such as
-template lookup and database instance.
-
-The blog.options will define the options and  parse the config.py.
-
-The blog.model will define the datamodel used by the app.
-"""
 import application
 import urls
 import handlers
