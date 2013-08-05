8aefd968aafdd70e80d310de2ae3fa1f71f8e5e0
RTNelo <rtnelo@yeah.net>
2013-08-05 14:39:17 +0800

    Add salt support to blog.utils.hash_repeat.

--- blog/utils.py
+++ blog/utils.py
@@ -5,15 +5,17 @@
 import hashlib
 
 
-def hash_repeat(raw, time=3):
+def hash_repeat(raw, salt_pre='', salt_suf='', time=3):
     """This function will multi-hash a string and return the hash value.
     args:
         raw(str): the string you want to get hash value.
+        salt_pre(str): the salt added before the raw.
+        salt_suf(str): the salt added after the raw.
         time(int): how many times hash process would you like.
     return(str):
         The hash value of raw.
     """
     res = raw
     for i in xrange(time):
-        res = hashlib.sha256(res).hexdigest()
+        res = hashlib.sha256(''.join((salt_pre, res, salt_suf))).hexdigest()
     return res
