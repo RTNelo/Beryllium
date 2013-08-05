00ea7e478d1d464e4fc63b99cd2923c5895f6ef5
RTNelo <rtnelo@yeah.net>
2013-08-05 12:34:16 +0800

    Modified the comment situation.

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-The urls module defines the urls used by the blog app.
+"""The urls module defines the urls used by the blog app.
 
 urls declare the relationship between the handlers and the urls. In another
 word, it defines a url should be handled by which handler.
 
 Now, the urls is empty yet. Because the handlers have been not writed.
 """
-import handlers  # This module defines the handlers used by the blog app.
+
+# This module defines the handlers used by the blog app.
+import handlers
 
 urls = [(),
         ]
