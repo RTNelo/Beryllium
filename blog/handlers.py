073b68d0e45a2cac9f11dafb4bb5d5062c756b11
RTNelo <rtnelo@yeah.net>
2013-08-05 12:29:34 +0800

    Simplify the docstring of handlers module by PEP257.

--- blog/handlers.py
+++ blog/handlers.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-The handlers module defines the Handlers used by the blog app.
+"""The handlers module defines the Handlers used by the blog app."""
 
-All handlers defined in this module is the subclass of the BaseHandler. The
-BaseHandler is extended from tornado.RequestHandler.
-
-"""
 from tornado import web
 
 import context
 
 
 class BaseHandler(web.RequestHandler):
-    """The superclass of all the other handlers defined in this module. defines
-    some common methods.
+    """The superclass of all Handler which will provide some common methods.
 
-    It shouldn't be used in the request handle.
+    This class shouldn't be used in the request handle behavior.
     """
     def initialize(self):
         """
         Override to prepare for the handler.
         """
-        #It still empty now.
+        #It's still empty now.
         pass
 
     def render_string(self, template_name, **kwargs):
         """
-        Override to provide mako templates support.
+        Override it to provide mako templates support.
         """
         template = context.template_lookup.get_template(template_name)
         return template.render(**kwargs)
