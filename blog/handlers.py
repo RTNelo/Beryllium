20064cdf12737300f7947c5960cdf6c7efcf2c06
RTNelo <rtnelo@yeah.net>
2013-08-06 18:09:02 +0800

    Add LoginHandler (without template).

--- blog/handlers.py
+++ blog/handlers.py
@@ -5,6 +5,7 @@
 from tornado import web
 
 import context
+import model
 
 
 class BaseHandler(web.RequestHandler):
@@ -57,3 +58,36 @@ class BaseHandler(web.RequestHandler):
             return self.session.value.user
         else:
             return None
+
+    def set_current_user(self, user):
+        """Set the current user.
+
+        args:
+            user(model.User): the current user you want to set.
+        """
+        self.session.value.user = user
+
+
+class LoginHandler(BaseHandler):
+    """Handler handle login request."""
+    def get(self):
+        """When receive a get request of login, render the login template."""
+        self.render('login.tpl')
+
+    def post(self):
+        """When receive a post request of login check it and display result.
+
+        If email and password is not None and a user can meet them, set the
+        current user and display a successful news. Or display a failed news.
+        """
+        try:
+            email = self.get_argument('email')
+            password = self.get_argument('password')
+        except web.MissingArgumentError:
+            self.render('login.failed.tpl')
+        user = model.User.get_user_by_email_and_password(email, password)
+        if user is None:
+            self.render('login.failed.tpl')
+        else:
+            self.set_current_user(user)
+            self.render('login.successful.tpl')
