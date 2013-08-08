65fc86913c3737dc80d92a86c4d26c4726dd7f97
RTNelo <rtnelo@yeah.net>
2013-08-08 14:38:30 +0800

    server.main will clean the context finally.

--- server.py
+++ server.py
@@ -9,15 +9,26 @@ from tornado import httpserver
 from tornado import ioloop
 
 from blog import application
+from blog import context
+
+
+def clean():
+    context.clean()
 
 
 def main():
-    """Main fuction which will create a HTTPServer listening the 80 port And
-    start it."""
-    http_server = httpserver.HTTPServer(application.Application())
-    http_server.listen(80)
-    print 'HTTPServer listening 80 will start now.'
-    ioloop.IOLoop.instance().start()
+    """Main fuction which will start a HTTPServer listening the 80 port
+
+    It also clean context finally. If you use the blog without start the main
+    fuction, please invoke the clean function yourself.
+    """
+    try:
+        http_server = httpserver.HTTPServer(application.Application())
+        http_server.listen(80)
+        print 'HTTPServer listening 80 will start now.'
+        ioloop.IOLoop.instance().start()
+    finally:
+        clean()
 
 if __name__ == '__main__':
     main()
