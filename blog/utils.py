f59c813cbc883783a90d4deb8d51dd4dc3937866
RTNelo <rtnelo@yeah.net>
2013-08-09 11:02:41 +0800

    Change docstring format in utils.py

--- blog/utils.py
+++ blog/utils.py
@@ -13,10 +13,14 @@ def hash_repeat(raw, salt_pre='', salt_suf='', time=3):
     """This function will multi-hash a string and return the hash value.
 
     args:
-        raw(str): the string you want to get hash value.
-        salt_pre(str): the salt added before the raw.
-        salt_suf(str): the salt added after the raw.
-        time(int): how many times hash process would you like.
+        raw(str):
+            The string you want to get hash value.
+        salt_pre(str):
+            The salt added before the raw.
+        salt_suf(str):
+            The salt added after the raw.
+        time(int):
+            How many times hash process would you like.
     return(str):
         The hash value of raw.
     """
@@ -30,8 +34,8 @@ def remove_microsecond(time):
     """Return a new datetime object from arg time that microsecond is 0.
 
     args:
-        time(datetime.datetime): which datetime object you want to remove
-                                 microsecond.
+        time(datetime.datetime):
+            Which datetime object you want to remove microsecond.
     return(datetime.datetime):
         A new datetime object same as arg time but microsecond is 0.
     """
@@ -42,8 +46,10 @@ def remove_microsecond(time):
 def content_convert(raw, converter=markdown2.markdown):
     """Convert the markdown content to content for visitor.
     args:
-        raw(str): raw content need convertint.
-        converter(callable): content_convert will return converter(raw).
+        raw(str):
+            Raw content need convertint.
+        converter(callable):
+            content_convert will return the result of converter(raw).
     return(str):
         The content value. Encoding is UTF-8.
     """
@@ -54,9 +60,12 @@ def apply_args_to_converter(converter, *args, **kwargs):
     """Apply args to a converter (callable).
 
     args:
-        converter(callable): the converter you want to apply args on.
-        *args: the args you want to apply.
-        **kwargs: the key-value args you want to apply.
+        converter(callable):
+            The converter you want to apply args on.
+        *args:
+            The args you want to apply.
+        **kwargs:
+            The key-value args you want to apply.
     return(functools.partial):
         A callable that equal to converter([args you provide], *args,
         **kwargs).
